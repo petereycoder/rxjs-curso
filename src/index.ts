5928fac5c7215f008ba5215f494ae6b454cd1cc9
petereycoder <fp.pipe.reyes@gmail.com>
2022-03-22 09:39:31 -0600

    observer y subscriber

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
+import { Observable, Observer } from 'rxjs';
 
-const nombre: string = 'Felipe Reyes';
-
-console.log(nombre);
-
-
-
-
-
-
-
+const observer: Observer<any> = {
+    next: value => console.log('next:', value),
+    error: error => console.warn('error', error),
+    complete: () => console.info('completado')
+}
